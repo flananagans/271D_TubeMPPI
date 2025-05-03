40a6a0a039dfd62e5585f6bb4673ee6bb00229f1
Will <wflan@ucla.edu>
2025-05-03 12:30:19 -0700

    adding comments

--- testcode_will/DiscreteLinearSystem.m
+++ testcode_will/DiscreteLinearSystem.m
@@ -1,9 +1,17 @@
 % Class for describing a discrete linear system with dynamics of the form
 %   x_{k+1} = Ax_{k} + Bu_{k}
+%
+%
+%   This currently only has the methods to update the state and add noise 
+%   to the control input from the control noise distribution 
+
 
 classdef DiscreteLinearSystem < handle
 
     properties
+        %% System description
+        % Right now this is the double integrator system from the paper
+        % with delta_t = 0.01
         A = [eye(2), eye(2)*0.01;
             zeros(2,2), eye(2)];
         x = zeros(4, 1);
@@ -11,9 +19,11 @@ classdef DiscreteLinearSystem < handle
              eye(2)*0.01];
         u = zeros(2, 1);
 
+        %% System limits
         u_lims = [-5, 5;
                   -5, 5]; % min and max allowable control
 
+        %% Noise definitions
         sigma_control = eye(2)/10; % control noise
         sigma_environment = eye(4)/10; % environmental noise
     end
@@ -21,7 +31,7 @@ classdef DiscreteLinearSystem < handle
     methods
         % Constructor
         function obj=DiscreteLinearSystem(varargin)
-            
+            % nothing for now
         end
 
         % Function to update the state of the system based on current x
@@ -32,15 +42,18 @@ classdef DiscreteLinearSystem < handle
 
         % Function to add control noise
         function addControlNoise(obj)
-            obj.u = obj.u + transpose(mvnrnd(zeros(size(obj.u)), obj.sigma_control));
 
+            % add noise from the control noise distribution
+            obj.u = obj.u + transpose(mvnrnd(zeros(size(obj.u)), ...
+                                             obj.sigma_control));
+
+            % apply control limits to the signal
             for m = 1:length(obj.u)
-                obj.u(m) = max([min([obj.u_lims(m, 2), obj.u(m)]), obj.u_lims(m, 1)]);
+                obj.u(m) = max([min([obj.u_lims(m, 2), obj.u(m)]), ...
+                                                obj.u_lims(m, 1)]);
             end
         end
-
     end
-
 end
 
 
--- testcode_will/testPt.m
+++ testcode_will/testPt.m
@@ -3,29 +3,32 @@ clear
 close all
 clc
 
-figure()
-scatter(0, 0, 5, 'k', 'filled');
-hold on
-
+%% Problem definition variables
 k = 100; % number of paths to investigate
 t = 25; % number of time steps
 
-pts = cell(1, k);
-xs = cell(size(pts));
+%% Initialize all of our systems
+pts = cell(1, k); % array of systems for each path
+xs = cell(size(pts)); % history of states for each system
 for j = 1:k
-    pts{j} = DiscreteLinearSystem();
+    pts{j} = DiscreteLinearSystem(); % construct the object
     pts{j}.x(3:4) = [0.25, 0.25]; % initial velocity
-    xs{j} = zeros(length(pts{j}.x), t);
+    xs{j} = zeros(length(pts{j}.x), t); % state history
 end
-for i = 1:t
 
+%% Roll trajectories forwards in time
+for i = 1:t
     for j = 1:k
-        pts{j}.addControlNoise();
-        pts{j}.updateState();
-        xs{j}(:, i) = pts{j}.x;
+        pts{j}.addControlNoise(); % add control noise to explore the space
+        pts{j}.updateState(); % update state to roll forward
+        xs{j}(:, i) = pts{j}.x; % record the current state
     end
 end
 
+%% Plot trajectories
+figure()
+scatter(0, 0, 5, 'k', 'filled');
+hold on
 for j = 1:k
     plot(xs{j}(1, :), xs{j}(2,:));
 end

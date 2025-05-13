960e058c074f92a07eb79b1d47bad0f85916d6b7
Will <wflan@ucla.edu>
2025-05-12 18:11:09 -0700

    Adding oval track with no obstacles

--- main/hw2_main.m
+++ main/hw2_main.m
@@ -11,13 +11,17 @@ K_MPPI = 1200; % number of trajectories to rollout
 T_MPPI = 2; % 2 second time-horizon
 v_des = 2; % m/s desired velocity
 
+% create MPPI controller instance and add control/environment noise
+
 % iLQG
 f_iLQG = 100; % frequency of the iLQG controller
 
-
+% create iLQG controller instance
 
 %% Simulation setup
 T_sim = 50; % time duration of simulation
+
+% create physical system model
 
 %% Create track
 track = StraightTrack();
@@ -25,11 +29,21 @@ track = StraightTrack();
 %% Run the simulation
 for t = 0:1/f_iLQG:T_sim
 
+    if(mod(t, floor(f_iLQG/f_MPPI)) == 0)
+        %% Update MPPI at a slower rate to get the current input
+        u_mppi = MPPI.computeInputs();
+    end
 
-    
+    %% Get input for this step from iLQG
+    u_ilqg = iLQG();
 
+    %% Update state of the actual system given the inputs from MPPI and iLQG
+    car.setControl(u_ilqg + u_mppi)
+    car.updateState()
 
-
-
-
+    %% Plot everything
+    track.plotTrack();
+    %MPPI.plot(); % function to plot all trajectories
+    %car.plot(); % plot position and orientation of our car
+    drawnow();
 end

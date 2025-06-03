5731b030bf019a10c4508551c0361ecb7b7eedb6
Niki Krockenberger <nikikrocken@MacBookAir.lan>
2025-06-02 17:51:05 -0700

    got rid of NaNs in derivatives

--- iLQG/iLQG_hw.m
+++ iLQG/iLQG_hw.m
@@ -4,7 +4,8 @@ classdef iLQG_hw
     properties
         car = []; %car object
         
-        lims = []; %control limits
+        lims = [-5 5;         % wheel angle limits (radians)
+             -5  5]; %control limits
         parallel = true; %use parallel line-search?
         Alpha = 10.^linspace(0,-3,11); %backtracking coefficients
         tolFun = 1e-7; %reduction exit critereon
@@ -761,8 +762,13 @@ classdef iLQG_hw
             [row col] = size(x);
             c = zeros(1, col);
             for i = 1:col
-                c(1,i) = x(1:4,i)'*obj.car.Q*x(1:4,i) + x(5:6,i)'*obj.car.R*x(5:6,i);
+                if isnan(x(5,i)) || isnan(x(6,i))
+                    c(1,col)  = x(1:4,col)'*obj.car.Q*x(1:4,col);
+                else
+                    c(1,i) = x(1:4,i)'*obj.car.Q*x(1:4,i) + x(5:6,i)'*obj.car.R*x(5:6,i);
+                end
             end
+            
             
         end
 
--- test.m
+++ test.m
@@ -4,7 +4,7 @@ close all
 car = DiscreteLinearSystem;
 ilqg = iLQG_hw(car);
 x0 = [10;5;0;0];
-u0 = .1*randn(2,1000);
+u0 = 10*randn(2,500);
 
 [x, u, L, Vx, Vxx, cost, trace, stop] = ilqg.solve(x0, u0);
 
@@ -12,3 +12,8 @@ plot(x(1,:), x(2,:))
 hold on
 plot(x(1,1), x(2,1), '*r')
 plot(x(1,end), x(2,end),'*b')
+
+figure;
+plot(u0(:,1), u0(:,2))
+hold on
+plot(u(:,1), u(:,2))
